da0456292fa573ff88ddb9194d3b7aaba23704ae
Pedro <pedro.migliatti@gmail.com>
2016-11-16 00:36:29 -0200

    Adicionado a primeira parte do script que gera o xml a partir do xls, por enquanto o script está apenas gerando o xml com um root e seus childs (ainda nao populado)

---
+++ xlsToXml.py
@@ -0,0 +1,34 @@
+from xlrd import open_workbook,XL_CELL_TEXT
+from lxml import etree
+
+#Abre a leitura do arquivo xls
+relations = open_workbook('relations.xls')
+#Seleciona o sheet desejado desse arquivo
+sheet = relations.sheet_by_index(0)
+
+#Define o root do arquivo xml
+root = etree.Element('Relation')
+#Comentario no xml
+comment1 = etree.Comment('Teste de criacao do XML')
+root.append(comment1)
+
+#Adicionando as childs ou SubElementos do root
+#O for percorre todas as colunas da primeira linha adicionando cada celula como child do root do xml
+for i in range(sheet.ncols):
+	child = sheet.cell_value(0,i)
+	child = etree.SubElement(root, child)
+	child.set('id', '100')
+
+#Coloca o root na arvore
+root_tree = etree.ElementTree(root)
+
+
+#Ainda nao faz merda nenhuma
+for i in range(sheet.nrows):
+	for j in range(sheet.ncols):
+		 sheet.cell_value(i,j)
+
+
+#Salva o arquivo xml
+outFile = open('homemade.xml', 'w')
+root_tree.write(outFile)
