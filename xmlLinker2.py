2be91b5589bb3c60269bef092d2bd1812a8ff4cd
felipequecole <felipequecole@msn.com>
2016-11-22 19:04:05 +0000

    Atualização no script para linkar as relações

--- xmlLinker2.py
+++ xmlLinker2.py
@@ -1,5 +1,6 @@
 import unicodedata
 import xmltodict
+from lxml import etree
 
 #Open the xml file
 raw_xml = open('homemade_v2.xml',  'r')
@@ -7,9 +8,20 @@ raw_xml = open('homemade_v2.xml',  'r')
 #Converts the xml to a dict 
 ontology = xmltodict.parse(raw_xml)
 
+related = {}
+
+
 #Exemplo de uso que pode ser util para nos
 for relation in ontology['Ontology']['Relation']:
+	aux = []
+	found = False
 	for relation2 in ontology['Ontology']['Relation']:
 		if (relation['relationName'] == relation2['generalizations'] and relation['relationName'] != 'relatedTo'):
-			print "'", relation2['relationName'], relation2['@id'], "'","can be generalized as","'",relation['relationName'], relation['@id'], "'"
-	
+			aux.append(int(relation2['@id']))
+			found = True
+			#print "'", relation2['relationName'], relation2['@id'], "'","can be generalized as","'",relation['relationName'], relation['@id'], "'"
+	if (found):
+		related[int(relation['@id'])] = aux
+
+
+print related 
