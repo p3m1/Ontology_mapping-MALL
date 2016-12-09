449d37f0379fbbc743c634c7e3b7d6492effa581
Felipe Quécole <felipequecole@msn.com>
2016-12-09 16:50:28 -0200

    Primeiro passo para linkagem: inserção de relationRef no campo 'generalizations'

--- xmlLinker2.py
+++ xmlLinker2.py
@@ -1,6 +1,7 @@
 import unicodedata
 import xmltodict
 from lxml import etree
+import xml.etree.ElementTree as ET
 
 #Open the xml file
 raw_xml = open('homemade_v2.xml',  'r')
@@ -8,7 +9,13 @@ raw_xml = open('homemade_v2.xml',  'r')
 #Converts the xml to a dict 
 ontology = xmltodict.parse(raw_xml)
 
-related = {}
+raw_xml.close()
+raw_xml = open('homemade_v2.xml',  'r')
+
+tree = ET.parse(raw_xml)
+root = tree.getroot()
+
+related = {} # chave eh generalizacao de elementos na lista
 
 
 #Exemplo de uso que pode ser util para nos
@@ -16,12 +23,25 @@ for relation in ontology['Ontology']['Relation']:
 	aux = []
 	found = False
 	for relation2 in ontology['Ontology']['Relation']:
-		if (relation['relationName'] == relation2['generalizations'] and relation['relationName'] != 'relatedTo'):
-			aux.append(int(relation2['@id']))
-			found = True
-			#print "'", relation2['relationName'], relation2['@id'], "'","can be generalized as","'",relation['relationName'], relation['@id'], "'"
-	if (found):
-		related[int(relation['@id'])] = aux
+		for gen in str(relation2['generalizations']).split():
+			if (relation['relationName'] == gen and relation['relationName'] != 'relatedTo'):
+				aux.append(int(relation2['@id']))
+				found = True
+				#print "'", relation2['relationName'], relation2['@id'], "'","can be generalized as","'",relation['relationName'], relation['@id'], "'"
+		if (found):
+			related[int(relation['@id'])] = aux
 
 
-print related 
+for generalization, especializations in related.items():	# coloca um relationRef onde tem uma generalizacao que eh relationName
+	for relation in root: 
+		for especialization in especializations: 
+			if (relation.attrib['id'] == str(especialization)):		# ele eh uma especializacao
+				aux = ET.SubElement(relation[4], 'relationRef', {'id':str(generalization)})
+
+outFile = open('homemade_linked.xml', 'w')
+tree.write(outFile)
+
+
+		
+
+#print related 
